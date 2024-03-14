25795d3882ec833e6ef668085decb8738372eb50
Deey-Kerry <dianahkerrynyash@gmail.com>
2024-03-14 20:47:22 +0000

    python_variable_annotations

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """type-annotated function make_multiplier"""
-from typing import Calllable
+from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:

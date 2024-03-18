d4dae28214b0589e0014c8a4fa5bd91ad79fa603
Deey-Kerry <dianahkerrynyash@gmail.com>
2024-03-18 20:42:17 +0000

    Python - Async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,22 +2,20 @@
 """async routine called wait_n that takes in 2 int arguments"""
 from typing import List
 import asyncio
-wait_random = __import__('0-basic_async_syntax.py').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """async routine called wait_n that takes in 2 int arguments
-    (in this order): n and max_delay
-    """
+    """async routine called wait_n that takes in 2 int arguments"""
     delays = []
-    tasks = []
+    p = []
+
     for i in range(n):
-        task = asyncio.create_task(wait_random(max_delay))
-        tasks.append(task)
-    while tasks:
-        done, tasks = await asyncio.wait(
-            tasks, return_when=asyncio.FIRST_COMPLETED)
-        for task in done:
-            delay = await task
-            delays.append(delay)
+        tasks = wait_random(max_delay)
+        p.append(tasks)
+
+    for tasks in asyncio.as_completed((p)):
+        delay = await tasks
+        delays.append(delay)
+
     return delays
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 """a measure_time function with integers n and max_delay"""
 import asyncio
-from time import perf_counter
-imported_func = __import__("1-concurrent_coroutines").wait_n
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
     """measures the total execution time for wait_n(n, max_delay),
     and returns total_time / n
     """
-    start_time = perf_counter()
-    asyncio.run(imported_func(n, max_delay))
-    end_time = perf_counter()
-    return (end_time - start_time)
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.time()
+
+    total = end - start
+    return (total/n)
